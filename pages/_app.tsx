31a4d110de8254b411252c4f93d08ef9d6c7bfbf
Sergio Padilha <sergio.padlha@gmail.com>
2023-08-03 08:58:50 -0300

    ajuste de import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import { Box, ChakraProvider } from '@chakra-ui/react'
 import { TransacoesFinanceirasProvider } from '../context/TransacoesFinanceirasContext';
 
